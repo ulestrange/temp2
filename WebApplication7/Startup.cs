bbe5f09ef78b3e77630cbf078c5c2eeab6c5db80
ulestrange <lestrange.una@itsligo.ie>
2019-03-11 17:18:18 +0000

    Playing with routes

--- WebApplication7/Startup.cs
+++ WebApplication7/Startup.cs
@@ -37,7 +37,7 @@ namespace WebApplication7
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebApplication7Context>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebApplication7Context")));
+                    options.UseInMemoryDatabase("mytempdb"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
